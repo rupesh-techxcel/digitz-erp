5625af5095a436954a4c88391173ee852db34522
Swapawar26 <swapnil.pawar@i2econsulting.com>
2023-03-08 11:42:45 +0530

    custom app

--- digitz_erp/accounts/doctype/payment_entry/payment_entry.js
+++ digitz_erp/accounts/doctype/payment_entry/payment_entry.js
@@ -3,6 +3,24 @@
 
 frappe.ui.form.on('Payment Entry', {
 	// refresh: function(frm) {
+		
+	// },
+});
 
-	// }
+frappe.ui.form.on('Payment Entry Detail', {
+	sup_or_expense_:function(frm, cdt, cdn) {
+		var row = locals[cdt][cdn];
+		console.log(row)
+		if (row.sup_or_expense_ == "Supplier"){
+			frappe.db.get_value("Company", "Dolphin Chemicals", "default_payable_account",(r) => {
+						var d = r.default_payable_account;
+						console.log(d,'------')
+						frappe.model.set_value(d.doctype, d.name, "account", r.default_payable_account)
+					});
+		}
+	},
+	account:function(frm, cdt, cdn) {
+		var row = locals[cdt][cdn];
+		console.log(row)
+	}
 });
