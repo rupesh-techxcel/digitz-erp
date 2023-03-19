6b99baf19c572d3bd892d407ec0869175c80652e
Rupesh P <rupesh@digitztech.com>
2023-03-19 14:39:30 +0530

    API for quotation

--- digitz_erp/api/quotation_api.py
+++ digitz_erp/api/quotation_api.py
@@ -4,5 +4,5 @@ from frappe.utils import get_datetime
 @frappe.whitelist()
 def get_quotation_data(quotation_no):	
     
-    return frappe.db.get_value("Quotation", quotation_no,["name","customer_name", "customer_address","reference_no","posting_date", "credit_sale"], as_dict=1)
+    return frappe.db.get_value("Quotation", quotation_no,["name","customer_name", "customer_address","reference_no","posting_date", "credit_sale", "tax_total","net_total", "rounded_total"], as_dict=1)
     
