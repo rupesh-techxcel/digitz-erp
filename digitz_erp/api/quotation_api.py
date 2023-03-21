6ce9f60394a527caaa5718856e7b3eacf52b6d07
rupeshamends <rupesh@amendscyber.com>
2023-03-21 13:32:03 +0000

    Db backup

--- digitz_erp/api/quotation_api.py
+++ digitz_erp/api/quotation_api.py
@@ -5,4 +5,13 @@ from frappe.utils import get_datetime
 def get_quotation_data(quotation_no):	
     
     return frappe.db.get_value("Quotation", quotation_no,["name","customer_name", "customer_address","reference_no","posting_date", "credit_sale", "tax_total","net_total", "rounded_total"], as_dict=1)
+
+@frappe.whitelist()
+def get_quotation_test():	    
+    return "Test Success"
+
+def get_quotation_items_data(quotation_no):	
     
+    return frappe.db.sql("select item, display_name,qty,unit,rate,tax_rate, tax_amount,net_amount from `tabQuotation Item` where parent=" + quotation_no, ignore_user_permission=True)
+    
+
