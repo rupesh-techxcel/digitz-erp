d12e7df5bc9c1a716bea0246aec99e3e68714819
Swapawar26 <swapnil.pawar@i2econsulting.com>
2023-03-30 22:21:49 +0530

    payment_allocation logic

--- digitz_erp/accounts/doctype/payment_entry/payment_entry.js
+++ digitz_erp/accounts/doctype/payment_entry/payment_entry.js
@@ -1,46 +1,147 @@
-// Copyright (c) 2022, Rupesh P and contributors
-// For license information, please see license.txt
-
 frappe.ui.form.on('Payment Entry', {
 	// refresh: function(frm) {
 
 	// }
 });
 
-
 frappe.ui.form.on("Payment Entry Detail", {
-	sup_and_exp:function(frm,cdt,cdn){
-		var row = locals[cdt][cdn]
-		if (row.sup_and_exp == 'Supplier'){
+	sup_and_exp: function(frm, cdt, cdn) {
+		var row = locals[cdt][cdn];
+		if (row.sup_and_exp == 'Supplier') {
 			frappe.db.get_value("Company", "Dolphin Chemicals", "default_payable_account").then((r) => {
-					var default_payable_account = r.message.default_payable_account
-					frappe.model.set_value(cdt,cdn,'account',default_payable_account)
-					
-			})
-		}if (row.sup_and_exp !="Supplier"){
-			frappe.model.set_value(cdt,cdn,'account',"")
-
+				var default_payable_account = r.message.default_payable_account;
+				frappe.model.set_value(cdt, cdn, 'account', default_payable_account);
+			});
+		}
+		else {
+			frappe.model.set_value(cdt, cdn, 'account', '');
 		}
 	},
-	supplier:function(frm,cdt,cdn){
-		frappe.call(
-			{
-				method: 'digitz_erp.accounts.doctype.payment_entry.payment_entry.create_dr_supplier_entry',
-				args:{doc:frm.doc},		
-				callback: (r) => {
-					frm.doc.payment_allocation = []
-					$.each(r.message,function(idx,elm){
-						var row = frm.add_child("payment_allocation");
-						row.purchase = elm.name
-						row.paid_amount = elm.gross_total
 
-					})
-					frm.refresh_field("payment_allocation")
-				}
-			});
+	supplier: function(frm, cdt, cdn) {
+		var row = locals[cdt][cdn];
+		console.log(row.supplier)
+		frappe.call({
+			method: 'digitz_erp.accounts.doctype.payment_entry.payment_entry.create_dr_supplier_entry',
+			args: { doc: frm.doc,
+				"supplier":row.supplier
+				},
+			callback: (r) => {
+				frappe.model.set_value(cdt, cdn, 'payment_entry_details', r.message);
+			}
+		});
+	},
 
+	payment_entry_details: function(frm, cdt, cdn) {
+        var row = locals[cdt][cdn];
+        var link_field_value = row.payment_entry_details;
+        var child_table_control;
 
-	}
+        if (link_field_value) {
+            frappe.call({
+                method: "frappe.client.get",
+                args: {
+                    doctype: "Payment entry Details",
+                    name: link_field_value,
+                },
+                callback(r) {
+                    if (r.message) {
+                        var doc = r.message;
+                        var child_table_data = doc.payment_allocation;
 
+                        child_table_control = frappe.ui.form.make_control({
+                            df: {
+                                fieldname: "payment_allocation",
+                                label: "Payment Allocation",
+                                fieldtype: "Table",
+                                cannot_add_rows:true,
+                                fields: [
+                                    {
+                                        fieldtype: "Link",
+                                        fieldname: "purchase",
+                                        label: "Purchase Invoice",
+                                        options:"Purchase Invoice",
+                                        in_place_edit: true,
+                                        in_list_view: true,
+                                        width: "100px",
+                                        read_only:true
+                                    },
+                                    {
+                                        fieldtype: "Float",
+                                        fieldname: "paid_amount",
+                                        label: "Paid Amount",
+                                        in_place_edit: false,
+                                        in_list_view: true,
+                                        width: "100px",
+                                        read_only:true
+                                    },
+                                    {
+                                        fieldtype: "Float",
+                                        fieldname: "invoice_ammount",
+                                        label: "Invoice Amount",
+                                        in_place_edit: false,
+                                        in_list_view: true,
+                                        width: "100px",
+                                        read_only:true
+                                    },
+                                    {
+                                        fieldtype: "Float",
+                                        fieldname: "balance_ammount",
+                                        label: "Balance Amount",
+                                        in_place_edit: false,
+                                        in_list_view: true,
+                                        width: "100px",
+                                        read_only:true
+                                    },
+                                    {
+                                        fieldtype: "Float",
+                                        fieldname: "pay",
+                                        label: "Pay",
+                                        in_place_edit: true,
+                                        in_list_view: true,
+                                        width: "100px",
+                                    }
+                                ],
+                            },
+                            parent: dialog.get_field("purchase").$wrapper,
+                            render_input: true,
+                        });
+                        child_table_control.df.data = child_table_data;
+                        child_table_control.refresh();
+                    }
+                }
+            });
+
+            var dialog = new frappe.ui.Dialog({
+                title: "Payment Allocation",
+                width: '100%',
+                fields: [
+                    {
+                        fieldtype: "HTML",
+                        fieldname: "purchase",
+                        label: "Payment Allocation",
+                        options: '<div id="child-table-wrapper"></div>',
+                    },
+                ],
+                primary_action: function() {
+                    var child_table_data_updated = child_table_control.get_value();
+                    console.log(child_table_data_updated,'child_table_data_updated')
+                    frappe.call({
+				        method: "digitz_erp.accounts.doctype.payment_entry.payment_entry.update_payment",
+				        args: {
+				            "child_table_data": child_table_data_updated
+				        },
+				        callback: function(response) {
+				        	dialog.hide();
+				            
+				        }
+				    });
+                },
+
+            });
+            dialog.show();
+        }
+    }
 
 });
+
--- digitz_erp/accounts/doctype/payment_entry/payment_entry.py
+++ digitz_erp/accounts/doctype/payment_entry/payment_entry.py
@@ -4,23 +4,47 @@
 import frappe
 from frappe.model.document import Document
 import json
+
+
 class PaymentEntry(Document):
 	pass
 
 
 
 @frappe.whitelist()
-def create_dr_supplier_entry(doc):
-	doc = json.loads(doc)
-	payment_entry = doc.get("payment_entry_details")
-	pur_invoice_lst = []
-	for idx,row in enumerate(payment_entry):
-		sup = row.get("supplier")
+def create_dr_supplier_entry(doc,supplier):
+	pur_invoice = frappe.db.sql("""SELECT name,gross_total,supplier,rounded_total,paid_amount FROM `tabPurchase Invoice` WHERE supplier = '{}' AND docstatus = 1 AND rounded_total != paid_amount""".format(supplier),as_dict=1)
 
-		pur_invoice = frappe.db.sql("""SELECT name,gross_total,supplier FROM `tabPurchase Invoice` WHERE supplier = '{}' AND docstatus = 0 """.format(sup),as_dict=1)
+	new_doc = frappe.new_doc('Payment entry Details')
+	for data in pur_invoice:
+		print(data.get('rounded_total'),'--',data.get('paid_amount'))
+		balance_amount = data.get('rounded_total') if data.get('paid_amount') == 0 else data.get('rounded_total') - data.get('paid_amount')
 
-		for row in pur_invoice:
-			pur_invoice_lst.append(row)
+		new_doc.append("payment_allocation",{
+					"paid_amount":data.get('gross_total'),
+					"purchase":data.get('name'),
+					"invoice_ammount":data.get('rounded_total'),
+					"balance_ammount":balance_amount
+					})
+		new_doc.save()
+	return new_doc.name
 
 
-	return pur_invoice_lst
+
+@frappe.whitelist()
+def update_payment(child_table_data):
+	data = json.loads(child_table_data)
+	for allocation in data:
+		print(allocation)
+		doc = frappe.get_doc("Purchase Invoice", allocation.get('purchase'))
+		paid_amount = doc.paid_amount + allocation.get('pay') or 0
+		doc.paid_amount = paid_amount
+
+		balance_amt = doc.rounded_total - doc.paid_amount
+		doc.balance_ammount = balance_amt
+
+		doc.save()
+
+
+
+	
