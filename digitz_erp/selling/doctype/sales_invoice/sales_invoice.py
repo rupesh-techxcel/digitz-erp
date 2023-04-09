511c3f25dca09e4bd029ea4b06e44b20f98de045
Rupesh P <rupesh@digitztech.com>
2023-04-09 16:06:06 +0530

    changes in sales invoice to update delivery note

--- digitz_erp/selling/doctype/sales_invoice/sales_invoice.py
+++ digitz_erp/selling/doctype/sales_invoice/sales_invoice.py
@@ -239,6 +239,9 @@ class SalesInvoice(Document):
         
         # if document already saved, do also already saved. so, if the user is submitting the sales invoice
         # just need to submit the do as well
+        
+        print("docstatus")
+        print(self.docstatus)
                 
         if self.docstatus == 1:
             si_do = frappe.get_doc('Sales Invoice Delivery Notes',{'parent':self.name})
@@ -250,6 +253,8 @@ class SalesInvoice(Document):
             return
         
         delivery_note_name = ""
+        
+        doNo = ""        
         
         si_name = self.name        
 
@@ -268,40 +273,139 @@ class SalesInvoice(Document):
                 do_exists = True
                 delivery_note_name =  frappe.db.get_value('Sales Invoice Delivery Notes',{'parent':self.name},['delivery_note'] )
                 # Remove the reference first before deleting the actual document
-                frappe.db.delete('Sales Invoice Delivery Notes',{'parent':self.name})
+                # frappe.db.delete('Sales Invoice Delivery Notes',{'parent':self.name})
                 delivery_note_doc = frappe.get_doc('Delivery Note', delivery_note_name)
-                delivery_note_name = delivery_note_doc.name
-                delivery_note_doc.delete()
-                print("delivery note deleted")
-       
-        delivery_note = self.__dict__
-        delivery_note['doctype'] = 'Delivery Note'
-        # delivery_note['against_sales_invoice'] = delivery_note['name']
-        # delivery_note['name'] = delivery_note_name        
-        delivery_note['naming_series'] = ""
-        delivery_note['posting_date'] = self.posting_date
-        delivery_note['posting_time'] = self.posting_time
+                doNo = delivery_note_doc.name
+                # delivery_note_doc.delete()
+                # print("delivery note deleted")
+                
+                delivery_note_doc.customer = self.customer
+                delivery_note_doc.customer_name = self.customer_name
+                delivery_note_doc.customer_display_name = self.customer_display_name                
+                delivery_note_doc.customer_address = self.customer_address
+                delivery_note_doc.reference_no = self.reference_no
+                delivery_note_doc.posting_date = self.posting_date
+                delivery_note_doc.posting_time = self.posting_time
+                delivery_note_doc.ship_to_location = self.ship_to_location
+                delivery_note_doc.salesman = self.salesman
+                delivery_note_doc.salesman_code = self.salesman_code
+                delivery_note_doc.tax_id = self.tax_id
+                delivery_note_doc.lpo_no = self.lpo_no
+                delivery_note_doc.lpo_date = self.lpo_date
+                delivery_note_doc.price_list = self.price_list
+                delivery_note_doc.rate_includes_tax = self.rate_includes_tax
+                delivery_note_doc.warehouse = self.warehouse
+                delivery_note_doc.credit_sale = self.credit_sale
+                delivery_note_doc.credit_days = self.credit_days
+                delivery_note_doc.payment_terms = self.payment_terms
+                delivery_note_doc.payment_mode = self.payment_mode
+                delivery_note_doc.payment_account = self.payment_account
+                delivery_note_doc.remarks = self.remarks
+                delivery_note_doc.gross_total = self.gross_total
+                delivery_note_doc.total_discount_in_line_items = self.total_discount_in_line_items
+                delivery_note_doc.tax_total = self.tax_total
+                delivery_note_doc.net_total = self.net_total
+                delivery_note_doc.round_off = self.round_off
+                delivery_note_doc.rounded_total = self.rounded_total
+                delivery_note_doc.terms = self.terms
+                delivery_note_doc.terms_and_conditions = self.terms_and_conditions
+                delivery_note_doc.auto_generated_from_delivery_note = True
+                delivery_note_doc.address_line_1 = self.address_line_1
+                delivery_note_doc.address_line_2 = self.address_line_2
+                delivery_note_doc.area_name = self.area_name
+                delivery_note_doc.country = self.country
+                delivery_note_doc.quotation = self.quotation
+                delivery_note_doc.sales_order = self.sales_order
+                
+                # Remove existing child table values
+                # frappe.db.sql("DELETE FROM `tabDelivery Note Item` where parent=%s", delivery_note_name)
+                # Manually update the sales invoice details
+               
+                
+                # Refresh document
+                # delivery_note_doc = frappe.get_doc('Delivery Note', delivery_note_name)
+                
+                # target_items = []
+                
+                # for item in self.items:
+                #     target_item = delivery_note_doc.append('items', {} )
+                #     frappe.copy_doc(item, target_item)
+                #     target_items.append(target_item)
+                    
+                delivery_note_doc.save()
+                # Remove existing child table values
+                frappe.db.sql("DELETE FROM `tabDelivery Note Item` where parent=%s", delivery_note_name)
+                
+                # target_items = []
+                
+                idx = 0
+                
+                for item in self.items:                                        
+                    idx = idx + 1
+                    delivery_note_item = frappe.new_doc("Delivery Note Item")
+                    delivery_note_item.warehouse = item.warehouse
+                    delivery_note_item.item = item.item
+                    delivery_note_item.item_code = item.item_code
+                    delivery_note_item.display_name = item.display_name
+                    delivery_note_item.qty =item.qty
+                    delivery_note_item.unit = item.unit
+                    delivery_note_item.rate = item.rate
+                    delivery_note_item.base_unit = item.base_unit
+                    delivery_note_item.qty_in_base_unit = item.qty_in_base_unit
+                    delivery_note_item.rate_in_base_unit = item.rate_in_base_unit
+                    delivery_note_item.conversion_factor = item.conversion_factor
+                    delivery_note_item.rate_included_tax = item.rate_included_tax
+                    delivery_note_item.rate_excluded_tax = item.rate_excluded_tax
+                    delivery_note_item.gross_amount = item.gross_amount
+                    delivery_note_item.tax_excluded = item.tax_excluded
+                    delivery_note_item.tax = item.tax
+                    delivery_note_item.tax_rate = item.tax_rate
+                    delivery_note_item.tax_amount = item.tax_amount
+                    delivery_note_item.discount_percentage = item.discount_percentage
+                    delivery_note_item.discount_amount = item.discount_amount
+                    delivery_note_item.net_amount = item.net_amount
+                    delivery_note_item.unit_conversion_details = item.unit_conversion_details
+                    delivery_note_item.idx = idx                    
+                    
+                    delivery_note_doc.append('items', delivery_note_item )                    
+                    #  target_items.append(target_item)
+                
+                delivery_note_doc.save()
+                
+                frappe.msgprint("Delivery Note for the Sales Invoice updated successfully.")
+                
+            else:
+                delivery_note = self.__dict__
+                delivery_note['doctype'] = 'Delivery Note'
+                # delivery_note['against_sales_invoice'] = delivery_note['name']
+                # delivery_note['name'] = delivery_note_name        
+                delivery_note['naming_series'] = ""
+                delivery_note['posting_date'] = self.posting_date
+                delivery_note['posting_time'] = self.posting_time
 
-        delivery_note['auto_generated_from_sales_invoice'] = 1
+                delivery_note['auto_generated_from_sales_invoice'] = 1
 
-        for item in delivery_note['items']:
-            item.doctype = "Delivery Note Item"    
-            item._meta = ""        
+                for item in delivery_note['items']:
+                    item.doctype = "Delivery Note Item"    
+                    item._meta = ""        
 
-        doNo = frappe.get_doc(delivery_note).insert()
-        frappe.db.commit()
-        
-        if(not do_exists):
-            delivery_note_name = doNo.name
+                delivery_note_doc = frappe.get_doc(delivery_note).insert()
+                frappe.db.commit()
+                delivery_note_name = doNo.name            
             
         # Rename the delivery note to the original dnoNo which is deleted
-        if(do_exists):
-            frappe.rename_doc('Delivery Note', doNo.name, delivery_note_name)
+        # if(do_exists):
+        #     frappe.rename_doc('Delivery Note', doNo.name, delivery_note_name)
         
         # do = frappe.get_doc('Delivery Note', delivery_note_name)
         si = frappe.get_doc('Sales Invoice',si_name)
-
-        row = si.append('delivery_notes', {'delivery_note': delivery_note_name})
+        
+        print("do_exists")
+        print(do_exists)
+        
+        # if frappe.db.exists('Sales Invoice Delivery Notes', {'parent': self.name}):    
+        if(not do_exists):
+            si.append('delivery_notes', {'delivery_note': delivery_note_name})
 
         si.save()
 
@@ -326,7 +430,7 @@ class SalesInvoice(Document):
         index = 0        
 
         for item in si.items:            
-            item.delivery_note_item_reference_no = doNo.items[index].name
+            item.delivery_note_item_reference_no = delivery_note_doc.items[index].name
             index = index + 1
 
         # Need to remove the next line to set auto_save_delivery_note
