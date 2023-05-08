7308ecf422e13605d0201ed578c89a6af81070bc
Deep Chirag <dpcg7125601@gmail.com>
2023-05-09 00:34:57 +0530

    Feat

--- digitz_erp/selling/doctype/tab_sale/tab_sale.js
+++ digitz_erp/selling/doctype/tab_sale/tab_sale.js
@@ -1,8 +1,24 @@
 // Copyright (c) 2023, Rupesh P and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Tab Sale', {
-	// refresh: function(frm) {
+frappe.ui.form.on("Tab Sale", {
+    onload: function (frm, cdt, cdn) {
+        frm.set_query("unit", "items", function () {
+            const d = locals[cdt][cdn]
+            console.log(d)
+            console.log(d.items)
+            d.items.map((data, index) => {
+                return {
+                    query: "digitz_erp.custom_query.get_item_wise_unit",
+                    filters: {
+                        "item": data.item
+                    }
+                };
+            })
+        });
+    }
+});
+frappe.ui.form.on("TabSaleInvoice", {
 
-	// }
+
 });
