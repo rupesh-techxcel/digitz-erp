35e8c75015700807d802ad25c5cc0a34296c75e3
Rupesh P <rupesh@digitztech.com>
2024-02-05 15:18:13 +0530

    gl report

--- digitz_erp/digitz_erp/report/general_ledger_report/general_ledger_report.js
+++ digitz_erp/digitz_erp/report/general_ledger_report/general_ledger_report.js
@@ -3,6 +3,51 @@
 
 frappe.query_reports["General Ledger Report"] = {
 	"filters": [
+		{		
+			"fieldname": "account",
+            "fieldtype": "Link",
+            "label": "Account",
+            "options": "Account",
+            "width": 200,
+            "get_query": function() {
+                return {
+                    filters: [
+                        ["is_group", "=", 0]  // Filter accounts where "is_group" is True
+                    ]
+                };
+            }
+		},
+		{		
+			"fieldname": "supplier",
+			"fieldtype": "Link",
+			"label": "Supplier",
+			"options": "Supplier",			      
+			"width": 200		
+		},
+		{		
+			"fieldname": "customer",
+			"fieldtype": "Link",
+			"label": "Customer",
+			"options": "Customer",			      
+			"width": 200		
+		},				
+		{		
+			"fieldname": "from_date",
+			"fieldtype": "Date",
+			"label": "From Date",						
+			"default":frappe.datetime.month_start()			
+		},
+		{		
+			"fieldname": "to_date",
+			"fieldtype": "Date",
+			"label": "To Date",			
+			"default":frappe.datetime.month_end()	
+		},
+		{		
+			"fieldname": "order_by_account",
+			"fieldtype": "Check",
+			"label": "Order by account"
+		},
 
 	]
 };
