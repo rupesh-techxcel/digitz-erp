4580571cfcddfcc7e23729d011cc561815ea4877
Rupesh P <rupesh@digitztech.com>
2024-03-05 12:56:25 +0530

    changes

--- digitz_erp/accounts/report/consolidated_tax_report/consolidated_tax_report.py
+++ digitz_erp/accounts/report/consolidated_tax_report/consolidated_tax_report.py
@@ -35,7 +35,8 @@ def get_sales_data(filters=None):
          SELECT
             CONCAT('Standard rated supplies in ', c.emirate) as label,
             ROUND(SUM(ts.gross_total),2) AS total_gross_total,
-            ROUND(SUM(ts.tax_total),2) AS total_tax_total
+            ROUND(SUM(ts.tax_total),2) AS total_tax_total,
+            emirate
         FROM
             `tabSales Invoice` ts
         INNER JOIN
@@ -60,6 +61,8 @@ def get_sales_data(filters=None):
     total_gross_total = round(total_gross_total,2)
     total_tax_total = round(total_tax_total,2)
 
+    data = fillEmirates(data)
+
     # Add the total row
     data.append({
         'label': 'Total',
@@ -67,16 +70,37 @@ def get_sales_data(filters=None):
         'total_tax_total': total_tax_total
     })    
     
+    total_gross_total = total_gross_total if total_gross_total else 0 
+    total_tax_total = total_tax_total if total_tax_total else 0
+    
 
     return data,total_gross_total,total_tax_total
 
+def fillEmirates(data):
+    emirate_list = ["Abu Dhabi", "Dubai", "Sharjah", "Ajman", "Umm Al Quwain", "Ras Al Khaimah", "Fujairah"]
+    
+    # Create a set of emirates already present in the data for O(1) lookup
+    existing_emirates = {sales_data["emirate"] for sales_data in data}
+    
+    for emirate in emirate_list:
+        if emirate not in existing_emirates:
+            data.append({
+                'emirate': emirate,  # Ensure you have a key for the emirate's name
+                'label': 'Standard rated supplies in ' + emirate,
+                'total_gross_total': 0.00,
+                'total_tax_total': 0.00
+            })
+                           
+    return data  
+
 def get_combined_data(filters=None):
+    
     sales_data, total_gross_sales,total_tax_sales = get_sales_data(filters)
     purchase_data = get_purchase_data(filters)
         
     # Calculate totals from the last row of purchase_data
-    total_gross_purchase = purchase_data[0]['total_gross_total']
-    total_tax_purchase = purchase_data[0]['total_tax_total']
+    total_gross_purchase = purchase_data[0]['total_gross_total'] if purchase_data else 0
+    total_tax_purchase = purchase_data[0]['total_tax_total'] if purchase_data else 0
 
     # Add Sl.No column to sales_data
     for idx, entry in enumerate(sales_data, start=1):
@@ -85,6 +109,11 @@ def get_combined_data(filters=None):
     # Add Sl.No column to purchase_data
     for idx, entry in enumerate(purchase_data, start=len(sales_data) + 2):
         entry['sl_no'] = idx
+
+    total_gross_purchase = total_gross_purchase if total_gross_purchase else 0
+    total_gross_sales = total_gross_sales if total_gross_sales else 0
+    total_tax_sales = total_tax_sales if total_tax_sales else 0
+    total_tax_purchase = total_tax_purchase if total_tax_purchase else 0
 
     # Create combined data including the calculated totals
     combined_data = sales_data + [{'label': ''}] + purchase_data + [
